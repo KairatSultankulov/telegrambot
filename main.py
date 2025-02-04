f1db192c45a3904a43ddb6ece6727581456a62d2
KairatSultankulov <sultankulov.kairat@gmail.com>
2025-02-04 20:22:07 +0600

    c

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+
+token = '8121863686:AAGqVoTNE69D8Xh2DCQ1HY5EEZe88HVeVDw'
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+
+async def main():
+    await dp.start_polling()
+
+if __name__ == '__main__':
+    asyncio.run(main())
