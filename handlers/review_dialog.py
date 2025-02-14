8ba12ec8373d6135d67c6532b8d9a852934d15ad
KairatSultankulov <sultankulov.kairat@gmail.com>
2025-02-15 01:03:55 +0600

    homework 4

--- handlers/review_dialog.py
+++ handlers/review_dialog.py
@@ -1,11 +1,12 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.types import Message, CallbackQuery
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from bot_config import database
 
 
-
-class RestourantRewiew(StatesGroup):
+class RestaurantReview(StatesGroup):
     name = State()
     phone_number = State()
     rate = State()
@@ -13,44 +14,56 @@ class RestourantRewiew(StatesGroup):
 
 
 async def start_dialog(callback: CallbackQuery):
-    await RestourantRewiew.name.set()
+    await RestaurantReview.name.set()
     await callback.message.answer('Как Вас зовут?')
+
+
+async def stop_dialog(message: Message, state: FSMContext):
+    await state.finish()
+    await message.answer('Спасибо за потраченное время!')
 
 
 async def process_name(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
-    await RestourantRewiew.next()
+    await RestaurantReview.next()
     await message.answer('Введите Ваш номер телефона')
 
 
 async def process_phone_number(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['phone_number'] = message.text
-    await RestourantRewiew.next()
+    await RestaurantReview.next()
     await message.answer('Поставьте оценку от 1 до 5')
 
 
 async def process_rate(message: Message, state: FSMContext):
-    if not message.text.isdigit() or not (1 <= int(message.text) <= 5):
-        await message.answer('Введите число от 1 до 5')
+    rate = message.text
+    if not rate.isdigit():
+        await message.answer('Введите число')
         return
-
+    rate = int(rate)
+    if rate < 1 or rate > 5:
+        await message.answer('Неподходящая оценка!')
+        return
     async with state.proxy() as data:
         data['rate'] = int(message.text)
-    await RestourantRewiew.next()
+    await RestaurantReview.next()
     await message.answer('Можете оставить дополнительные коментарии')
 
 
 async def process_extra_comments(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['extra_comments'] = message.text
+    data = await state.get_data()
+    database.add_review(data)
     await message.answer('Спасибо за отзыв!')
     await state.finish()
 
 def register_handlers(dp: Dispatcher):
     dp.register_callback_query_handler(start_dialog, lambda c: c.data == 'feedback')
-    dp.register_message_handler(process_name, state=RestourantRewiew.name)
-    dp.register_message_handler(process_phone_number, state=RestourantRewiew.phone_number)
-    dp.register_message_handler(process_rate, state=RestourantRewiew.rate)
-    dp.register_message_handler(process_extra_comments, state=RestourantRewiew.extra_comments)
+    dp.register_message_handler(stop_dialog, Text(equals=('stop', 'стоп'), ignore_case=True), state='*')
+    dp.register_message_handler(process_name, state=RestaurantReview.name)
+    dp.register_message_handler(process_phone_number, state=RestaurantReview.phone_number)
+    dp.register_message_handler(process_rate, state=RestaurantReview.rate)
+    dp.register_message_handler(process_extra_comments, state=RestaurantReview.extra_comments)
--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import asyncio
 import logging
 
-from bot_config import dp
+from bot_config import dp, database
 from handlers import (
     start,
     myinfo,
     names,
     branches,
     delivery,
-    rewiew_dialog
+    review_dialog
 )
 
 
 async def main():
     start.register_handlers(dp)
-    rewiew_dialog.register_handlers(dp)
+    review_dialog.register_handlers(dp)
     myinfo.register_handlers(dp)
     names.register_handlers(dp)
     branches.register_handlers(dp)
     delivery.register_handlers(dp)
+    database.create_tables()
     await dp.start_polling()
 
 
